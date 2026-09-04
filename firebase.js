08eede0f4958284eccd119a448c1a570c35e45ef
precious1nasty-debug <precious1nasty@gmail.com>
2026-09-04 11:42:10 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -6,45 +6,26 @@ import {
   getFirestore
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
+import {
+  getAuth
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
 const firebaseConfig = {
-
-  apiKey:
-    "AIzaSyBbknvIO-87ff0lOCSUHtJlPs_CyhkTDnE",
-
-  authDomain:
-    "dls-competition.firebaseapp.com",
-
-  projectId:
-    "dls-competition",
-
-  storageBucket:
-    "dls-competition.firebasestorage.app",
-
-  messagingSenderId:
-    "855283628558",
-
-  appId:
-    "1:855283628558:web:c7c4335e13da16a8775a5f",
-
-  measurementId:
-    "G-1CFJFVHBXZ"
-
+  apiKey: "AIzaSyBbknvIO-87ffl0OCSUHtJlPs_CyhkTDnE",
+  authDomain: "dls-competition.firebaseapp.com",
+  projectId: "dls-competition",
+  storageBucket: "dls-competition.firebasestorage.app",
+  messagingSenderId: "855283628558",
+  appId: "1:855283558:web:c7c4335e13da16a8775a5f",
+  measurementId: "G-1CFJFVHBXZ"
 };
 
+const app = initializeApp(firebaseConfig);
 
-const app =
-  initializeApp(
-    firebaseConfig
-  );
+const db = getFirestore(app);
 
+const auth = getAuth(app);
 
-const db =
-  getFirestore(app);
-
-
-window.db =
-  db;
-
-window.firebaseReady =
-  true;
+window.db = db;
+window.auth = auth;
+window.firebaseReady = true;
