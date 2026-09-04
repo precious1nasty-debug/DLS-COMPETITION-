51a62e05edb36ea2952087041425acb04cbd03bf
precious1nasty-debug <precious1nasty@gmail.com>
2026-09-04 13:04:21 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -10,21 +10,24 @@ import {
   getAuth
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBbknvIO-87ffl0OCSUHtJlPs_CyhkTDnE",
+  apiKey: "AIzaSyBbknvIO-87ff0lOCSUHtJlPs_CyhkTDnE",
   authDomain: "dls-competition.firebaseapp.com",
   projectId: "dls-competition",
   storageBucket: "dls-competition.firebasestorage.app",
   messagingSenderId: "855283628558",
-  appId: "1:855283558:web:c7c4335e13da16a8775a5f",
+  appId: "1:855283628558:web:c7c4335e13da16a8775a5f",
   measurementId: "G-1CFJFVHBXZ"
 };
+
 
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
 
 const auth = getAuth(app);
+
 
 window.db = db;
 window.auth = auth;
