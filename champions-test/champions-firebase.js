80a5189914e83b878c7ffc7dd6729f398b0ffe4f
precious1nasty-debug <precious1nasty@gmail.com>
2026-09-25 14:13:37 +0100

    Update champions-firebase.js

--- champions-test/champions-firebase.js
+++ champions-test/champions-firebase.js
@@ -1,7 +1,6 @@
 /* =========================================================
    DLS CHAMPIONS LEAGUE
-   champions-firebase.js
-   PART 4 — FIREBASE CONNECTION
+   FIREBASE CONNECTION
    ========================================================= */
 
 import {
@@ -17,62 +16,24 @@ import {
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
 
-/* =========================
-   FIREBASE CONFIGURATION
-========================= */
-
 const firebaseConfig = {
-
   apiKey: "AIzaSyBbknvIO-87ff0lOCSUHtJlPs_CyhkTDnE",
-
-  authDomain:
-    "dls-competition.firebaseapp.com",
-
-  projectId:
-    "dls-competition",
-
-  storageBucket:
-    "dls-competition.firebasestorage.app",
-
-  messagingSenderId:
-    "855283628558",
-
-  appId:
-    "1:855283628558:web:c7c4335e13da16a8775a5f",
-
-  measurementId:
-    "G-1CFJFVHBXZ"
-
+  authDomain: "dls-competition.firebaseapp.com",
+  projectId: "dls-competition",
+  storageBucket: "dls-competition.firebasestorage.app",
+  messagingSenderId: "855283628558",
+  appId: "1:855283628558:web:c7c4335e13da16a8775a5f",
+  measurementId: "G-1CFJFVHBXZ"
 };
 
 
-/* =========================
-   INITIALIZE FIREBASE
-========================= */
-
 const app = initializeApp(firebaseConfig);
 
-
-/* =========================
-   FIRESTORE
-========================= */
-
 const db = getFirestore(app);
-
-
-/* =========================
-   AUTHENTICATION
-========================= */
 
 const auth = getAuth(app);
 
 
-/* =========================
-   GLOBAL REFERENCES
-========================= */
-
 window.championsDb = db;
-
 window.championsAuth = auth;
-
 window.championsFirebaseReady = true;
