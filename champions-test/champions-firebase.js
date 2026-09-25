c9dad5c871ad4b104b0387836b6fcf662a26a190
precious1nasty-debug <precious1nasty@gmail.com>
2026-09-25 14:29:04 +0100

    Update champions-firebase.js

--- champions-test/champions-firebase.js
+++ champions-test/champions-firebase.js
@@ -16,24 +16,80 @@ import {
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
 
+/* =========================
+   FIREBASE CONFIG
+========================= */
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBbknvIO-87ff0lOCSUHtJlPs_CyhkTDnE",
-  authDomain: "dls-competition.firebaseapp.com",
-  projectId: "dls-competition",
-  storageBucket: "dls-competition.firebasestorage.app",
-  messagingSenderId: "855283628558",
-  appId: "1:855283628558:web:c7c4335e13da16a8775a5f",
-  measurementId: "G-1CFJFVHBXZ"
+
+  apiKey:
+    "AIzaSyBbknvIO-87ff0lOCSUHtJlPs_CyhkTDnE",
+
+  authDomain:
+    "dls-competition.firebaseapp.com",
+
+  projectId:
+    "dls-competition",
+
+  storageBucket:
+    "dls-competition.firebasestorage.app",
+
+  messagingSenderId:
+    "855283628558",
+
+  appId:
+    "1:855283628558:web:c7c4335e13da16a8775a5f",
+
+  measurementId:
+    "G-1CFJFVHBXZ"
+
 };
 
 
-const app = initializeApp(firebaseConfig);
+/* =========================
+   INITIALIZE FIREBASE
+========================= */
 
-const db = getFirestore(app);
+const app =
+  initializeApp(firebaseConfig);
 
-const auth = getAuth(app);
+
+/* =========================
+   FIRESTORE
+========================= */
+
+const db =
+  getFirestore(app);
 
 
-window.championsDb = db;
-window.championsAuth = auth;
-window.championsFirebaseReady = true;
+/* =========================
+   AUTHENTICATION
+========================= */
+
+const auth =
+  getAuth(app);
+
+
+/* =========================
+   GLOBAL REFERENCES
+========================= */
+
+window.championsDb =
+  db;
+
+window.championsAuth =
+  auth;
+
+window.championsFirebaseReady =
+  true;
+
+
+/* =========================
+   READY EVENT
+========================= */
+
+window.dispatchEvent(
+  new CustomEvent(
+    "championsFirebaseReady"
+  )
+);
