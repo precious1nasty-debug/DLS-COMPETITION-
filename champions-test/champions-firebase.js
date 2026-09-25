125e3f86af79c33cf14aeaa08504b4f8131495ed
precious1nasty-debug <precious1nasty@gmail.com>
2026-09-25 12:54:25 +0100

    Update champions-firebase.js

--- champions-test/champions-firebase.js
+++ champions-test/champions-firebase.js
@@ -18,17 +18,31 @@ import {
 
 
 /* =========================
-   FIREBASE CONFIG
+   FIREBASE CONFIGURATION
 ========================= */
 
 const firebaseConfig = {
+
   apiKey: "AIzaSyBbknvIO-87ff0lOCSUHtJlPs_CyhkTDnE",
-  authDomain: "dls-competition.firebaseapp.com",
-  projectId: "dls-competition",
-  storageBucket: "dls-competition.firebasestorage.app",
-  messagingSenderId: "855283628558",
-  appId: "1:855283628558:web:c7c4335e13da16a8775a5f",
-  measurementId: "G-1CFJFVHBXZ"
+
+  authDomain:
+    "dls-competition.firebaseapp.com",
+
+  projectId:
+    "dls-competition",
+
+  storageBucket:
+    "dls-competition.firebasestorage.app",
+
+  messagingSenderId:
+    "855283628558",
+
+  appId:
+    "1:855283628558:web:c7c4335e13da16a8775a5f",
+
+  measurementId:
+    "G-1CFJFVHBXZ"
+
 };
 
 
